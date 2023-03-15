369c78d64ddf0b04cc42d0b5f9115e67613a613c
Dai Doan <doanvandai2012@gmail.com>
2023-03-15 23:11:31 +0700

    [structure] add bottom sheet

--- app/src/main/java/com/example/compose/jetchat/recentchat/RecentChatFragment.kt
+++ app/src/main/java/com/example/compose/jetchat/recentchat/RecentChatFragment.kt
@@ -5,8 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -14,6 +26,7 @@ import com.example.compose.jetchat.NavGraphDestinations
 import com.example.compose.jetchat.R
 import com.example.compose.jetchat.data.exampleRecentChatUiState
 import com.example.compose.jetchat.theme.JetchatTheme
+import kotlinx.coroutines.launch
 
 class RecentChatFragment : Fragment() {
 
@@ -39,12 +52,63 @@ class RecentChatFragment : Fragment() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecentCallScreen(navController: NavController) {
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
+    var skipPartiallyExpanded by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val bottomSheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = skipPartiallyExpanded
+    )
+
     RecentChatContent(
         exampleRecentChatUiState,
         onOpenConversation = {
             navController.navigate(NavGraphDestinations.Home)
+        },
+        onNewChat = {
+            skipPartiallyExpanded = true
+            openBottomSheet = ! openBottomSheet
         }
     )
+
+    // Sheet content
+    if (openBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { openBottomSheet = false },
+            sheetState = bottomSheetState,
+            modifier = Modifier.padding(top = 24.dp),
+            scrimColor = Color.White.copy(0.32f)
+        ) {
+            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Button(
+                    // Note: If you provide logic outside of onDismissRequest to remove the sheet,
+                    // you must additionally handle intended state cleanup, if any.
+                    onClick = {
+                        scope.launch { bottomSheetState.hide() }.invokeOnCompletion {
+                            if (!bottomSheetState.isVisible) {
+                                openBottomSheet = false
+                            }
+                        }
+                    }
+                ) {
+                    Text("Hide Bottom Sheet")
+                }
+            }
+            LazyColumn {
+                items(50) {
+                    ListItem(
+                        headlineContent = { Text("Item $it") },
+                        leadingContent = {
+                            Icon(
+                                Icons.Default.Favorite,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    }
 }
